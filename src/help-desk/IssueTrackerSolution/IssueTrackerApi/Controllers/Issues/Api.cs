8e322bda91ef951dce6c058bf49b927388ffd475
Mario Rodriguez <Mario_Rodriguez_36@progressive.com>
2024-04-23 16:43:08 -0400

    End of Day push!

--- src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using IssueTrackerApi.Services;
 using Marten;
 using Microsoft.AspNetCore.Mvc;
 
@@ -65,6 +66,52 @@ public class Api(IDocumentSession session) : ControllerBase
             return BadRequest(results.ToDictionary()); // 400
         }
     }
+
+    [HttpPost("/issues-rpc")]
+    public async Task<ActionResult> AddIssueWithRpcAsync([FromBody] CreateIssueRequestModel request,
+        [FromServices] IValidator<CreateIssueRequestModel> validator,
+        [FromServices] SupportHttpClient client)
+    {
+        var results = await validator.ValidateAsync(request);
+
+        if (results.IsValid)
+        {
+            var response = new Issue
+            {
+                CreatedAt = DateTime.UtcNow,
+                Description = request.Description,
+                Software = request.Software,
+                Id = Guid.NewGuid(),
+                Status = IssueStatus.Created
+            };
+
+            session.Store(response);
+            await session.SaveChangesAsync();
+
+            var supportInfo = await client.GetCurrentSupportInformatiaonAsync();
+
+            if (supportInfo != null)
+            {
+                var res = new IssueWithEmbeddedSupport
+                {
+                    Id = response.Id,
+                    CreatedAt = response.CreatedAt,
+                    Description = response.Description,
+                    Software = response.Software,
+                    Status = IssueStatus.Created,
+                    Support = supportInfo
+                };
+
+                return Ok(res);
+            }
+
+            return Ok(response);
+        }
+        else
+        {
+            return BadRequest(results.ToDictionary()); // 400
+        }
+    }
 }
 
 public record CreateIssueRequestModel
@@ -80,10 +127,17 @@ public record Issue
     public string Software { get; set; } = string.Empty;
     public DateTimeOffset CreatedAt { get; set; }
     public IssueStatus Status { get; set; }
-    public SupportInfo Support { get; set; } = new("Bob Smith", "Bob@aol.com", "555-1222");
 }
 
-public record SupportInfo(string Name, string Email, string Phone);
+public record IssueWithEmbeddedSupport
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Software { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public IssueStatus Status { get; set; }
+    public CurrentSupportResponse Support { get; set; } = new();
+}
 
 public enum IssueStatus { Created }
 
---
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Services/SupportHttpClient.cs
@@ -0,0 +1,27 @@
+﻿namespace IssueTrackerApi.Services;
+
+public class SupportHttpClient
+{
+    private readonly HttpClient _httpClient;
+
+    public SupportHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<CurrentSupportResponse?> GetCurrentSupportInformatiaonAsync()
+    {
+        var response = await _httpClient.GetAsync("/");
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadFromJsonAsync<CurrentSupportResponse>();
+        return content;
+    }
+}
+
+public record CurrentSupportResponse
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
